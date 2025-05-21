0dd30b1b97fb6b4c91949abcec11d7059fab36e2
Rayyan Khan <ryankhann47@gmail.com>
2025-05-21 13:47:10 -0400

    removed test comments, wsl is chill now

--- windows/game.cpp
+++ windows/game.cpp
@@ -2,8 +2,6 @@
 #include <Windows.h>
 using namespace std;
 
-// Test comment
-// Test comment 2, git y u do dis
 
 wstring tetromino[7]; // The tetromino itself
 int nFieldWidth = 12; // The playing field
